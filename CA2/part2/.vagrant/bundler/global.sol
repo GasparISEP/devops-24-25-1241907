50e9a0a77b0ae6527d5f12800290d7e3aa5de832
gasparfigueiras <1241907@isep.ipp.pt>
2025-05-08 14:54:49 +0100

    Trying to run war instead of jar.(#21)

--- CA2/part2/.vagrant/bundler/global.sol
+++ CA2/part2/.vagrant/bundler/global.sol
@@ -1,1 +1,1 @@
-{"dependencies":[["vagrant-qemu",["= 0.3.9"]]],"checksum":"47e26f7d1e602459af3db64ecaed33ce2596fa400fdc8cc020985a477c29499f","vagrant_version":"2.4.3"}
+{"dependencies":[["vagrant-qemu",["= 0.3.11"]]],"checksum":"a6902ea9c34ab10f75a55f947028caeeca2d80e69d6795485ae6297a72dd2df4","vagrant_version":"2.4.5"}
